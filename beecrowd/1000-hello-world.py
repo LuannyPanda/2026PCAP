0919bb219f4123438f898b7f0cdb1a976a999cd0
LuannyPanda <20261ctb0100037@estudantes.ifpr.edu.br>
2026-04-09 20:00:57 +0000

    Aula 11 - 1009:salario com bonus

---
+++ beecrowd/1000-hello-world.py
@@ -0,0 +1,15 @@
+'''
+Problema: beecrowd / 1000
+Data: 2026.04.07
+Estudante: Luanny Cubas Ramos Silva
+'''
+# Objetivo: Exibir exatamente a mensagem "Hello World!" na tela
+
+# ---ANÁLISE (LIAC) ---
+# Entrada: nenhuma - este problema não requer leitura de dados
+# Processamento: nenhum - não há cálculo ou transformação
+# Saída: exibir o texto "Hello World!" exatamente como especificado
+
+# print() é o comando de saída em Python
+# Equivalente ao bloco "diga..." do Scratch
+print('Hello World!')
---
+++ beecrowd/1000.py
@@ -0,0 +1,12 @@
+'''
+Problema: beecrowd / 1000
+Data: 2026.04.07
+Estudante: Luanny Cubas Ramos Silva
+'''
+# Objetivo: Imprimir a mensagem "Hello World!"
+
+
+# ---ANÁLISE (LIAC) ---
+# Entrada: 
+# Processamento:
+# Saída:
