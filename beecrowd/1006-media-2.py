00bd73a8b486c56d9375e9b5c38f7405bab47ae5
LuannyPanda <20261ctb0100037@estudantes.ifpr.edu.br>
2026-05-07 20:04:41 +0000

    Aula 11 - 1006: media 2

--- beecrowd/1006-media-2.py
+++ beecrowd/1006-media-2.py
@@ -14,6 +14,6 @@ A = float(input())
 B = float(input())
 C = float(input())
 
-media = (A * 2 + B * 3 + C * 5)/1.0
+media = (A * 2 + B * 3 + C * 5) / 10
 
-print(f"MEDIA={media:.1}")
+print(f"MEDIA = {media:.1f}")
